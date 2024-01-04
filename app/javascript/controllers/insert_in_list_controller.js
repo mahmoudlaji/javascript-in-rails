c52a6a03a82745dd8ba455399a849172f1baef2c
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-04 22:34:38 +0100

    jbuilder

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -7,16 +7,21 @@ export default class extends Controller {
 
     
     send(event) {
-      event.preventDefault();
+      // [...]
     
       fetch(this.formTarget.action, {
-        method: "POST", // Could be dynamic with Stimulus values
+        method: "POST",
         headers: { "Accept": "application/json" },
         body: new FormData(this.formTarget)
       })
         .then(response => response.json())
         .then((data) => {
-          console.log(data)
+          if (data.inserted_item) {
+            // beforeend could also be dynamic with Stimulus values
+            this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+          }
+          this.formTarget.outerHTML = data.form
         })
     }
+    
 }
