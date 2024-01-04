c34a925a96800fd7307c129c1bceb30fd8fb95be
lajimi mahmoud <mahmoudladjimi@gmail.com>
2024-01-04 21:14:05 +0100

    add dateflatp

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
-    console.log("olla rahmouchhtii")
+  static targets = ["togglableElement"]
+
+  
+  fire() {
+    this.togglableElementTarget.classList.toggle("d-none");
   }
 }
